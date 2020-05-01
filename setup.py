93a34fd5568af3e765c9f4a0e70d385f6e5749da
Day Barr <github.com@daybarr.com>
2020-05-01 11:46:39 +0100

    Fix for py3

--- setup.py
+++ setup.py
@@ -12,16 +12,16 @@ if __name__ == "__main__":
         home_file = os.path.join(home_dir, dotfile)
         repo_file = os.path.join(this_dir, dotfile)
         if not os.path.lexists(home_file):
-            print "  Linking {0} => {1}".format(home_file, repo_file)
+            print("  Linking {0} => {1}".format(home_file, repo_file))
             os.symlink(repo_file, home_file)
             continue
         if not os.path.islink(home_file):
-            print "WARNING: {0} already exists".format(home_file)
+            print("WARNING: {0} already exists".format(home_file))
             continue
         real_home_file = os.path.realpath(home_file)
         real_repo_file = os.path.realpath(repo_file)
         if real_home_file != real_repo_file:
-            print "WARNING: {0} already links elsewhere: {1}".format(
-                home_file, os.path.realpath(home_file))
+            print("WARNING: {0} already links elsewhere: {1}".format(
+                home_file, os.path.realpath(home_file)))
         else:
-            print "  Done: {0} => {1}".format(home_file, repo_file)
+            print("  Done: {0} => {1}".format(home_file, repo_file))
